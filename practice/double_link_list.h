f5afd0f162032873903491b36fc9407ef5223dc2
“david” <david@signority.com>
2024-05-02 22:29:16 -0700

    May 2 final version

--- practice/double_link_list.h
+++ practice/double_link_list.h
@@ -23,8 +23,9 @@ namespace dll {
 
     template<typename T>
     void reverse_print(node<T> *back) {
+        std::cout << "reverse order ";
         while (back != nullptr) {
-            std::cout << back->key << "\t";
+            std::cout << back->key << "->";
             back = back->prev;
         }
         std::cout << std::endl;
@@ -43,24 +44,62 @@ namespace dll {
         node<T> *new_node = new node<T>{key, back, back->prev};
         back->prev->next = new_node;
         back->prev = new_node;
-        return back;
+        return new_node;
     }
 
     template<typename T>
-    node<T> *insert_in_place(node<T> *&front, node<T> *&back, node<T> *&p) {
-        // find out which node should insert before it
-        dll::node<int> *target = back->prev;
+    void append(node<T> *&tail, T key) {
+        node<T> *new_node = new node<T>{key, tail, tail->prev};
+        tail->prev->next = new_node;
+        new_node->next = tail;
+        tail->prev = new_node;
+    }
+
+    template<typename T>
+    node<T> *insert_in_place(node<T> *&front, node<T> *&p) {
+//        std::cout << "insert p=" << p->key << std::endl;
+        // find out which node should insert behind it
+        node<T> *target = p->prev;
         while (target != front and target->key > p->key) {
             target = target->prev;
         }
-
-        target->next->prev = p;
-        p->next = target->next;
+//        std::cout << "after target " << target->key << std::endl;
+        node<T> *temp = p;
+        node<T> *temp_left = p->prev;
+        node<T> *temp_right = p->next;
+        node<T> *target_right = target->next;
+        if (p->key == target->next->key) {
+            return front;
+        } else {
+//            std::cout << "1 " << p->key << ".next=" << target->next->key << std::endl;
+            p->next = target->next;
+        }
+//        std::cout << "2 " << target->key << ".next=" << p->key << std::endl;
         target->next = p;
-        p->prev = target;
+//        std::cout << "3 " << temp_left->key << ".next=" << temp_right->key << std::endl;
+        temp_left->next = temp_right;
+//        std::cout << "4 " << temp->key << ".prev=" << target->key << std::endl;
+        temp->prev = target;
+//        std::cout << "5 " << target_right->key << ".prev=" << temp->key << std::endl;
+        target_right->prev = temp;
+//        std::cout << "6 " << temp_right->key << ".prev=" << temp_left->key << std::endl;
+        temp_right->prev = temp_left;
         return front;
     }
+
+    template<typename T>
+    void insertion_sort(node<T> *&front, node<T> *&end) {
+        if (front == end || front == nullptr || end == nullptr) {
+            return;
+        }
+
+        for (auto i = front->next->next; i != end; i = i->next) {
+//            std::cout << "i=" << i->key << std::endl;
+            insert_in_place(front, i);
+        }
+    }
 }
+
 
 #define PRACTICE_DOUBE_LINK_LIST_H
 
--- practice/main.cpp
+++ practice/main.cpp
@@ -15,7 +15,7 @@ int main() {
     back->prev = my_list;
 
 
-    for (int i = 0; i < 10; i++) {
+    for (int i = 0; i < 4; i++) {
         push_front(my_list, u(e));
     }
 
@@ -35,20 +35,60 @@ int main() {
     new_node->prev = ptr;
 **/
     //test
+//    insertion_sort(front, back);
     list_print(front);
 //    reverse_print(back);
 
-
-    auto new_front = new dll::node<int>{};
-    auto new_back = new dll::node<int>{};
-    dll::node<int> *sorted_list = new dll::node<int>{100, new_back, new_front};
-    new_front->next = sorted_list;
-    new_back->prev = sorted_list;
-    insert_in_place(new_front, new_back, front->next->next);
-    list_print(new_front);
-//    for (auto i = front->next->next; i != nullptr; i = i->next) {
-//        insert_in_place(new_front, new_back, i);
+//    auto copy_front = new dll::node<int>{}; //fake node
+//    auto copy_back = new dll::node<int>{}; //fake node
+//    dll::node<int> *copy = new dll::node<int>{front->next->key, copy_back, copy_front};
+//    copy_front->next = copy;
+//    copy_back->prev = copy;
+//
+//    for (auto i = front->next->next; i->next != nullptr; i = i->next) {
+//        dll::append(copy_back, i->key);
 //    }
-    insert_in_place(new_front, new_back, front->next->next->next);
-    list_print(new_front);
+
+
+//    auto new_front = new dll::node<int>{};
+//    auto new_back = new dll::node<int>{};
+//    dll::node<int> *sorted_list = new dll::node<int>{front->next->key, new_back, new_front};
+//    new_front->next = sorted_list;
+//    new_back->prev = sorted_list;
+
+
+//    front = insert_in_place(front, front->next->next);
+//    list_print(front);
+//    reverse_print(back);
+//
+//    insert_in_place(front, front->next->next);
+//    list_print(front);
+//    reverse_print(back);
+//
+//    insert_in_place(front, front->next->next->next);
+//    list_print(front);
+//    reverse_print(back);
+//
+//    insert_in_place(front, front->next->next->next->next);
+//    list_print(front);
+//    reverse_print(back);
+//
+//    insert_in_place(front, front->next->next->next->next->next);
+//    list_print(front);
+//    reverse_print(back);
+
+//
+//    insert_in_place(front, front->next->next->next->next->next);
+//    list_print(front);
+//    reverse_print(back);
+
+
+
+    insertion_sort(front, back);
+    list_print(front);
+    reverse_print(back);
+
+
+//    insert_in_place(front, front->next->next->next);
+//    list_print(new_front);
 }
